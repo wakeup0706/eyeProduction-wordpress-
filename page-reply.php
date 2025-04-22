f6a7e46e5edf8e6ff65df590abf8239e3792e2b1
wakeup0706 <lemanhhung0706+1@gmail.com>
2025-04-23 04:02:05 +0900

    [main]: vision good possiply

---
+++ page-reply.php
@@ -0,0 +1,228 @@
+<?php  get_header();?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/Pages/visionDictionaryDetail.css" />
+    <main class="single-all_articles">
+        <div class="container">
+            <div class="breadcrumb"><span><a href="<?php echo home_url(); ?>">TOP</a></span><span>> <a href="<?php echo site_url();?>/visiondictionary">視力向上可能性判定結果</a></span></div>
+            <div class="kindleBooksDetail-content flex space-between">
+                <div class="flex-left">
+                    <div class="title flex space-between">
+                        <div class="text">視力向上可能性判定結果</div>
+                        <div class="sns-content flex">
+                            <div><a href="https://twitter.com/meikusouken2001" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/sns1.png" alt=""></a></div>
+                            <div><a href="https://www.instagram.com/dreamteam2001.12.17/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/sns2.png" alt=""></a></div>
+                            <div><a href="https://www.youtube.com/channel/UCN0xlurLZi2E3m2_Nl5DT8Qhttps://www.youtube.com/channel/UCN0xlurLZi2E3m2_Nl5DT8Q" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/sns3.png" alt=""></a></div>
+                            <div><a href="https://www.facebook.com/Dreamteam-Inc-988153384589165/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/sns4.png" alt=""></a></div>
+                        </div>
+                    </div>
+                    <div class="banner"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/backgrounds/reply.jpg" alt="null" /></div>
+                    
+                    <div id="first" class="infor">
+                        <p class="infor-title">視力向上可能性判定結果</p>
+                        <div class="infor-reply">
+                            <br/>
+                            <p>まず右目に関しましては、次のように考えられます。</p>
+                            <p>『視力回復の余地はあるものの、トレーニングで得られる効果には、上限があると考えられる状態です。』</p>
+                            <p>日常生活でメガネがなくても支障がない視力は、両眼で測定した場合の視力が0.7以上です。（左右それぞれの測定で視力0.5以上）</p>
+                            <p>仮に、生活環境から視力低下の原因となっている要素が取り除けて、視力低下の勢いを止められたとしても、トレーニングでメガネの必要のない視力まで持って行くことは難しいと思われます。</p>
+                            <p>これ以上、大きな視力としての変動はありませんが、近視自体は進行し続けますので、現状維持を目標に生活改善やトレーニングをすることをお勧めします。</p>
+                            <p>一方、左目に関しましては、次のように考えられます。<br/>『視力低下していますが、現段階であればトレーニングによって、日常生活でメガネの必要のないレベルに視力を向上させ、維持させる可能性がある状態です。』</p>
+                            <p>日常生活でメガネがなくても支障がない視力は、両眼で測定した場合の視力が0.7以上です。（左右それぞれの測定で視力0.5以上）</p>
+                            <p>現在、ギリギリか下回っている状況です。</p>
+                            <p>まずは、これを余裕を持ってクリアすることを目標にして頂けると良いと思います。</p>
+                            <p>目標値実現のためには、これ以上近視を進行させないことが、とても重要です。</p>
+                            <p>まずは、生活環境から近視の原因となっている要素を取り除き、近視に向かっている勢いを止めてしまう必要があります。<br/>その上で、ホームワックでピントの調節力を上げることにより、視力を向上させていきます。</p>
+                            <p>なお、屈折度数は屈折検査により測定される値で、メガネ処方箋に値が掲載されます。<br/>メガネを作る作らないに関わらず、目の状態を知るための重要な値になりますので、確認をお勧めします。</p>
+                            <p>ちなみに、眼鏡をまだ作らない場合は処方箋は出されませんが、眼科医は、屈折検査の結果データを持っています。<br/>その中の屈折度数（球面レンズ・Sph）、乱視度数（円柱レンズ・Cyl)、矯正視力の値をご確認ください。<br/>親切な眼科なら、電話でも教えてくれる例が多いようです。</p>
+                            <p>眼科だけではなく、大手メガネチェーン店などでも屈折検査を無料でしてくれるところがありますので、測定だけされてみてはいかがでしょうか？<br/>その値をご入力頂ければ、より精度の高い判定となります。</p>
+                            <p>左右の視力差が0.3以上ある場合は、目について知ろうの不同視をご参照ください。</p>
+                            <p>遠視の度数にもよりますが、年齢が8歳以上の方の場合は、残念ながらトレーニングで効果を出す事は難しいと言わざるを得ません。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/megane.png" alt="infor-reply" style="float:left;padding:0.5em">
+                            <p>視力によっては、すでに<span>メガネやコンタクトレンズを使用</span>しているかもしれませんが、メガネは<span>見えない距離を見る時だけ</span>使うのが基本です。<br/>ですが、視力が<span>極端に落ちている場合</span>は、手元のものを見る時にもメガネの必要な場合もあるかもしれません。</p>
+                            <p>その際は、以前に使用していた<span>度の弱いメガネ</span>があれば、それを使って<span>近くの作業をする</span>という手もあります。<br/>距離によって、違う度のメガネを使い分ける方法です。</p>
+                            <p>常にメガネやコンタクトをするほどでもない方でも、例えば学校の黒板が見えないなど、<span>視力によっては必要なケース</span>が出てくると思われます。</p>
+                            <p>このような場合は学校と相談して、<span>席を前の方に</span>替えてもらうと良いと思います。<br/>それでも見えなくて<span>支障が出る場合<span>は、メガネを使用して下さい。</p>
+                            <p>このように、メガネやコンタクトを使用する際は<span>「見えない距離を見る必要がある時だけ」</span>、使うようにして下さい。</p>
+                            <p>裸眼で<span>見える距離</span>の場合は、なるべくメガネやコンタクトに頼らずに<span>目の筋肉を使う機会を多く</span>する方が、望ましいです。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/telescope.png" alt="infor-reply" style="float:right;padding:0.5em">
+                            <p>極端な例えですが、<span>「メガネは望遠鏡として使う」</span>という意識でいると、手元の本やスマホなどを見る時は、メガネを外そうと心がける事ができます。</p>
+                            <p>また、逆にメガネを<span>使わないことにこだわり</span>すぎて、<span>「遠くのものを目を細めて見る癖」</span>をつけてしまうことも、良くありません。<br/>
+                            「目を細める癖」は、<span>乱視を進行させる可能性</span>があるので、注意が必要です。</p>
+                            <p>メガネやコンタクトの調整度数は、<span>両眼で0.8程度、片眼で0.6程度</span>出ていれば充分です。<br/>
+                            遠方で矯正を強くすると、近方では矯正が強すぎる弊害が出やすくなってしまいます。</p>
+                            <p>メガネやコンタクトは、使い方にさえ気をつけていただければ、全くダメと言う訳ではありません。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/camera.png" alt="infor-reply" style="float:left;padding:0.5em">
+                            <p><span>目を細める癖</span>が出ているようですが、これは目をカメラにたとえるなら、絞りを強くしている状態です。</p>
+                            <p>目を細めることによって目に入る光の量を調節して、<span>網膜に映る像のボケを少なく</span>しているからです。</p>
+                            <p>こういった理由で、目を細めるとピントのボケがなくなるので、より鮮明に遠くが見えます。</p>
+                            <p>ですが、本来は遠くを見るときは毛様体筋を緩めて水晶体を薄くし、焦点を合わせます。</p>
+                            <p>目を細めるというのは、<span>目の周りの筋肉に力が加わり</span>ますので、これとは逆の方法です。
+                            そういう意味では、目の自然な筋肉の動きを邪魔しています。</p>
+                            <p>目を細める癖が頻繁に出てくると、眼圧が高くなり角膜に変形を起こし<span>「乱視」になる場合</span>があります。</p>
+                            <p>乱視は一度なってしまうと、治すのは難しくやっかいです。</p>
+                            <p>目を細めることは癖になりやすいので、なるべく早い時期に直した方が良いです。</p>
+
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/tv.png" alt="infor-reply" style="float:right;padding:0.5em">
+                            <p>また、<span>テレビを見るときの距離</span>は遠くありませんか？</p>
+                            <p>適切なテレビの距離ですが、<span>リラックスしてピントの合わせられる一番遠い距離</span>が、目の負担を軽減できる距離と言えます。</p>
+                            <p>これは<span>「最大明視距離(めいしきょり)」</span>と呼ばれる距離です。</p>
+                            <p>最大明視距離とは、ご自身がピントを合わせられるギリギリの距離のことを言います。</p>
+                            <p>一言で「遠く」とは言っても、その「遠く」とは、ひとりひとり異なります。</p>
+                            <p>ですので、まずはご<span>自身にあった「遠く」を見極める</span>必要があります。</p>
+                            <p>ご自身の最大明視距離を感覚的につかむ為に、以下のような実験をしてみると良いでしょう。</p>
+                            <p>壁に紙を貼るなどして、その紙に書かれた文字がふつうに読める位置に立ち、その位置から少しずつ後ろへ下がっていってください。<br/>
+                            そして、<span>ピントのぼやけ始める距離</span>をつかんで下さい。</p>
+                            <p>そうすると、初めのうちはピントを合わせようと動いていた目の筋肉が、ある距離を境に調節しなくなるのが分かると思います。</p>
+                            <p>これが、<span>目の筋肉の動くギリギリの距離＝最大明視距離</span>であり、それぞれの方に適した「遠く」となります。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/relux2.png" alt="infor-reply" style="float:left;padding:0.5em">
+                            <p>目を細めるという行為は<span>精神的な側面も</span>あり、見えないと不安に思ってしまう事で、目を細めてしまう場合があります。</p>
+                            <p>ぼんやりとしか見えていない状態を、「今、目の筋肉がリラックスしているところだな。<br/>
+                            ぎゅっと縮こまっていたのが、解放されてホッとしているな。」という風に思うと、捉え方が変わってくると思います。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/stress1.png" alt="infor-reply" style="float:right;padding:0.5em">
+                            <p>眼球自体に<span>全く異常がない</span>のに関わらず、<span>よく見えないケース</span>というのは、<span>心因性による視力障害の可能性</span>も考えられます。</p>
+                            <p>これは、脳の問題と考えられていまして、<span>強度のストレス</span>からくる視覚野（脳の見ることに関係する領域）への血流障害などが原因となっていると考えられます。</p>
+                            <p>その場合、心因性のストレスがなくなったら全く症状がなくなったり、度のないメガネをかけたら視力が向上したりするなど、一風変わった現象が起こります。</p>
+                            <p>この場合は、視力の悪さと比較して、屈折度数の異常が極端に少ないケースとなります。<br/>
+                            上記ケースは<span>非常に珍しい</span>ですが、<span>ストレスによる視力低下</span>は年々増えています。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/stress2.png" alt="infor-reply" style="float:left;padding:0.5em">
+                            <p>ストレスによる視力低下として多いのが、<span>「調節緊張」</span>と呼ばれるケース。</p>
+                            <p>人間は、精神的なストレスが強いと全身の筋肉が収縮しがちになります。<br/>
+                            それが、毛様体という目の<span>ピント調節にも関わる筋肉</span>について起こると、遠くを見るときもその収縮が解けず、<span>ピントが合わなく</span>なってしまいます。</p>
+                            <p>このような現象を、「調節緊張」と言います。<br/>
+                            これは<span>ストレスから生じる</span>場合と、近くを<span>長時間凝視するため</span>に生じる場合の、二つの理由がありますが、その両方が一緒になって起こっているケースもあります。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/megusuri.png" alt="infor-reply" style="float:right;padding:0.5em">
+                            <p>このケースでは、視力でだけでなく屈折度数も見かけ上悪くなります。<br/>
+                            そして、屈折度数の測定時にミドリンなどの<span>筋肉弛緩剤</span>の目薬を使うと、<span>値が改善されやすく</span>なります。</p>
+                            <p>しかし、それは<span>あくまでその時だけ</span>ですので、根本的な<span>ストレスや凝視の習慣</span>などを取り除かないと、<span>根本的な解決には</span>なりません。</p>
+                            <p>ですので、このような現象が起こらぬよう、精神的にゆとりを持って暮らせる配慮が必要です。</p>
+                            <p>それでは、<span>目の効果的な休憩法</span>を下記にまとめます。</p>
+                            <p>集中力の強い方は、脇目もふらずに没頭する傾向があり、<span>目の筋肉を収縮させた状態で長時間</span>使ってしまいがちですので、ぜひお試しください。</p>
+                            <p style="font-weight: blod">視力ランドが教えるワンポイントレッスン♪</p>
+
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/halftime.png" alt="infor-reply" style="float:left;padding:0.5em">
+                            <p>目の休憩法【ハーフタイム】</p>
+                            <p>読書、スマホ、パソコン、勉強、ゲームなど、<span>近くのものをみる作業</span>（近業）を10～30分間したら、「遠くの違う距離のものを眺める」という休憩を入れる。</p>
+                            <p><span>休憩時間は10～30秒ほど</span>で結構ですので、近くを見る作業を長時間続けずに、<span>こまめに休憩を</span>いれる事が大切です。</p>
+                            <ul>
+                                <li>・裸眼で行って下さい</li>
+                                <li>・自分が裸眼で「リラックスしてピントが合わせられる距離」で、一番遠い所を見て下さい。</li>
+                                <li>・瞬きも入れると、さらに効果的です。</li>
+                            </ul>
+                            <p>ちまたでよく<span>「遠くの緑を見る」</sapn>などと言われますが、その方法には<span>一長一短</span>があります。</p>
+                            <p>遠くの距離が、まるで<span>ピント調整のできない距離</span>の場合、目の筋肉は反応しないため、筋肉への<span>刺激は不十分</span>となります。</p>
+                            
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/relux.png" alt="infor-reply" style="float:right;padding:0.5em">
+                            <p>ですので、目の良い方にとって「遠くの緑を見る」ことは休憩になりますが、視力の落ちている方にとっては、<span>休憩にならない場合が</span>あります。</p>
+                            <p>このような理由で、ハーフタイムは<span>ピントの合わせられる距離</span>で行うのが効果的です。</p>
+                            <p>テレビを見る時は、リラックスしてピントが合わせられるギリギリの所に、椅子やソファなどを置いて専用の場所を作るという工夫をすると良いです。<br/>
+                            また、CMの時には瞬きをしたり、<span>目の体操</span>をする習慣を身に付けると更に効果的です。</p>
+
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/timer.png" alt="infor-reply" style="float:left;padding:0.5em">
+                            <p>注意すべきは、DVDなどで映画を見る時です。<br/>
+                            CMがないので、ついつい長時間続けて見てしまいがちです。<br/>
+                            このような時は、100円ショップなどで売られている<span>デジタルタイマーやスマホのタイマー機能</sapn>などを活用して、10～30分毎に強制的に見る事をやめて、<span>目を休める事</span>を忘れないでください。</p>
+                            <p>視力ランドでは、検査から判定以外にも視力を守る方法を色々ご紹介しております。<br/>
+                            ぜひ、サイト内を色々見て回って下さい。</p>
+                        </div>
+                        <div class="infor-reply infor-reply-attend">
+                            <p>トレーニングにご興味を持たれたら、楽しくなければ視力回復じゃない♪<br/>
+                                自宅でテレビを見ながらできる新発想の視力回復トレーニング「ホームワック」がお勧め！</p>
+                            <ul>
+                                <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/bnr_homewac_01.jpg" alt="infor-reply"></a></li>
+                                <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/bnr_homewac_02.jpg" alt="infor-reply"></a></li>
+                                <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/bnr_homewac_03.jpg" alt="infor-reply"></a></li>
+                            </ul>
+                        </div>
+                        <div class="infor-reply infor-reply-attend">
+                            <p class="single">視力向上可能性判定限定！</p>
+                            <p>ホームワック1週間お試しレンタル1,000円 → <span>500円</span></p>
+                            <p>※レンタルお申込みの際は、今回ご入力頂いたメールアドレスでお申込み下さい。<br/>
+                                メールアドレスが違いますと、限定特典が適用にならない場合がございます。<br/>
+                                ※レンタルにあたりましては、公的身分証のご提示が必要となります。<br/>
+                                ※お申込者様が未成年の場合は、親権者様の同意書と公的身分証明書が必要です。<br/>
+                                同意書は<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/reply/consent.pdf" target="_blank">こちら</a>からダウンロード（PDF）できます。</p>
+                        </div>
+                        <div class="infor-reply infor-reply-footer">
+                            <p>更に詳しくご相談をご希望の方は、<a href="<?php echo site_url();?>/inquiry">お問い合わせフォーム</a>よりご連絡ください。</p>
+                            <p>※本判定は、これまでのトレーニング実績などのデータに基づき、判定を行っています。<br/>
+                                ※海外での検査数値をそのまま入れた場合、正確に判定ができない場合がございます。<br/>
+                                <a href="<?php echo site_url();?>/inquiry">お問い合わせフォーム</a>よりお知らせ頂ければ、判定いたします。<br/>
+                                ※効果・効能には個人差があります。</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-right">
+                    <div class="title"><p>視力向上可能性判定結果</p></div>
+                    <div class="banner"><a href="https://www.heallite.com/c/desklight/gentlite/M0002" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/land_banner1.jpg" alt=""></a></div>
+                    <div class="otherCategory">
+                        <div class="otherCategory-item">
+                            <a href="<?php echo site_url();?>/visionpossibility">
+                                <div class="otherCategory-item-img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/iconResult.png" alt=""></div>
+                                <div class="otherCategory-item-text">視力向上可能性判定</div>
+                            </a>
+                        </div>
+                        <div class="otherCategory-item">
+                            <a href="<?php echo site_url();?>/eyeglossary">
+                                <div class="otherCategory-item-img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/iconDic.png" alt=""></div>
+                                <div class="otherCategory-item-text">目の用語辞典</div>
+                            </a>
+                        </div>
+                        <div class="otherCategory-item">
+                            <a href="<?php echo site_url();?>/recipes">
+                                <div class="otherCategory-item-img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/iconFood.png" alt=""></div>
+                                <div class="otherCategory-item-text">目に優しいレシピ</div>
+                            </a>
+                        </div>
+                        <div class="otherCategory-item">
+                            <a href="<?php echo site_url();?>/notice">
+                                <div class="otherCategory-item-img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Icons/iconNotice.png" alt=""></div>
+                                <div class="otherCategory-item-text">お知らせ</div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="banner"><a href="https://line.me/ti/p/@959ejwxf" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/line-registration.jpg" alt=""></a></div>
+                    <div class="searchBox">
+                        <div class="search-Button">
+                            <input type="text" placeholder="Search.." name="search" id="hot_search_detail">
+                            <button class="btn_hot_search_detail">検 索</button>
+                        </div>
+                    </div>
+                    <div class="categoryTag">
+                        <p>おすすめタグ</p>
+                        <div>
+                            <a href="#" class="side-tag-list-trick">#お金</a>
+                            <a href="#" class="side-tag-list-trick">#カードゲーム</a>
+                            <a href="#" class="side-tag-list-trick">#かず</a>
+                            <a href="#" class="side-tag-list-trick">#カタカナ</a>
+                            <a href="#" class="side-tag-list-trick">#クイズ</a>
+                            <a href="#" class="side-tag-list-trick">#ゲーム</a>
+                            <a href="#" class="side-tag-list-trick">#こうさく</a>
+                            <a href="#" class="side-tag-list-trick">#シール</a>
+                            <a href="#" class="side-tag-list-trick">#ちえ</a>
+                            <a href="#" class="side-tag-list-trick">#なぞなぞ</a>
+                            <a href="#" class="side-tag-list-trick">#パズル</a>
+                            <a href="#" class="side-tag-list-trick">#ひらがな</a>
+                            <a href="#" class="side-tag-list-trick">#図鑑</a>
+                            <a href="#" class="side-tag-list-trick">#好奇心</a>
+                            <a href="#" class="side-tag-list-trick">#実験</a>
+                            <a href="#" class="side-tag-list-trick">#工作</a>
+                            <a href="#" class="side-tag-list-trick">#文字</a>
+                            <a href="#" class="side-tag-list-trick">#科学遊び</a>
+                            <a href="#" class="side-tag-list-trick">#算数</a>
+                            <a href="#" class="side-tag-list-trick">#色彩感覚</a>
+                            <a href="#" class="side-tag-list-trick">#言葉</a>
+                            <a href="#" class="side-tag-list-trick">#語彙力</a>
+                            <a href="#" class="side-tag-list-trick">#遊び</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php get_footer(); ?>
